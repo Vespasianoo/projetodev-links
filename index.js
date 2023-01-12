fa93bf3ce49505c0770b334eec63bf053b860870
Matheus Vespasiano <vespasianomatheus@gmail.com>
2023-01-12 13:14:08 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function toggleMode(){
+    const html = document.documentElement
+    html.classList.toggle("light")
+    
+    const img = document.querySelector("img")
+    if(html.classList.contains("light")) {
+        img.setAttribute("src", "./assets/light-img.jpg")
+    } else {
+        img.setAttribute("src", "./assets/avatar.png")
+    }
+}
